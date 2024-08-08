db6c5fec9e1edb30297afc0b0f00a2c40c379101
angelo <gelo.guimba@gmail.com>
2024-08-08 21:47:32 +0800

    add routes and auth middleware to source

--- src/app.js
+++ src/app.js
@@ -4,6 +4,11 @@ import mongoose from 'mongoose';
 import path from 'path';
 import { fileURLToPath } from 'url';
 
+import { authenticate } from './middlewares/authMiddleware.js';
+
+import web from './routes/web.js';
+import api from './routes/api.js';
+
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
@@ -20,6 +25,15 @@ app.use("/js", express.static("./node_modules/bootstrap/dist/js"));
 app.use("/css",express.static("./node_modules/bootstrap/dist/css"));
 app.use('/bootstrap-icons', express.static('./node_modules/bootstrap-icons'));
 
+// Routes
+app.use(web);
+app.use('/api/', api);
+
+app.get('/', (request, response) => response.redirect('/home'));
+app.get('/home', authenticate, (request, response) => response.sendFile(path.join(__dirname, 'public', 'home.html')))
+
+app.use((request, response) => response.status(404).sendFile(path.join(__dirname, 'public', '404.html')));
+
 (async () => {
     try {
         const uri = 'mongodb://mongo:27017/app';
@@ -28,7 +42,7 @@ app.use('/bootstrap-icons', express.static('./node_modules/bootstrap-icons'));
         await mongoose.connect(uri);
         
         console.log('Database connected');
-        
+
         app.listen(port, () => console.log(`Server running on port ${port}`));
     
     } catch (error) {
