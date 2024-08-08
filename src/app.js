b3dbe4ac8153c7ac027e7859b738ed34a52841f4
angelo <gelo.guimba@gmail.com>
2024-08-08 21:23:35 +0800

    setup basic express middleware and bootstrap

---
+++ src/app.js
@@ -0,0 +1,32 @@
+import express from 'express';
+import cookieParser from 'cookie-parser';
+import mongoose from 'mongoose';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const app = express();
+
+// Configs
+app.use(express.static('src/public'));
+
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+app.use(cookieParser());
+
+app.use("/js", express.static("./node_modules/bootstrap/dist/js"));
+app.use("/css",express.static("./node_modules/bootstrap/dist/css"));
+app.use('/bootstrap-icons', express.static('./node_modules/bootstrap-icons'));
+
+(async () => {
+    try {
+        const port = 3000;
+        
+        app.listen(port, () => console.log(`Server running on port ${port}`));
+    
+    } catch (error) {
+        console.log(error);
+    }
+})();
