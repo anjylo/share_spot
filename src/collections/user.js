0d96c08f4e8bbed8935d7f78ae89b72e8aba0466
angelo <gelo.guimba@gmail.com>
2024-08-08 21:27:47 +0800

    add user schema

---
+++ src/collections/user.js
@@ -0,0 +1,53 @@
+import mongoose from 'mongoose';
+import bcrypt from 'bcrypt';
+
+const { Schema } = mongoose;
+
+// Define structure of a document
+const userSchema = new Schema({
+    email: {
+        type: String,
+        required: [true, 'Email required'],
+        unique: [true, 'Email is already taken'],
+        lowercase: true,
+        validate: {
+            validator: (email) => /^[A-Za-z0-9._]+@[A-Za-z]+\.[A-Za-z]+(\.[A-Za-z]+)*$/.test(email),
+            message: 'Invalid email format'
+        },
+    },
+    username: {
+        type: String,
+        required: [true, 'Username required'],
+        unique: [true, 'Username is already taken'],
+    },
+    password: {
+        type: String,
+        required: [true, 'Password required'],
+        minLength: [8, 'Minumum password length is 8 characters']
+    },
+    role: {
+        type: String,
+        required: false,
+        default: 'user',
+        enum: {
+            values: ['admin', 'user'],
+            message: '{VALUE} is not supported'
+        }
+    }
+}, { timestamps: false });
+
+userSchema.pre('save', async function (next) {
+    const salt = await bcrypt.genSalt();
+    this.password = await bcrypt.hash(this.password, salt);
+    next();
+});
+
+userSchema.pre('findOneAndUpdate', async function (next) {
+    const salt = await bcrypt.genSalt();
+    this._update.password = await bcrypt.hash(this._update.password, salt);
+    next();
+});
+
+const User = mongoose.model('User', userSchema);
+
+export default User;
